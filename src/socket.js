e9766e8acb46841028e077f0079030dd6f236591
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2024-02-16 01:49:13 +0530

    UPD: Bug fixes

--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,6 @@
 import { io } from "socket.io-client";
-// const dotenv = require("dotenv");
-
-// dotenv.config({
-//   path: "@/.env.local",
-// });
-
-const isProd =
+const URL =
   process.env.NODE_ENV === "production"
-    ? "https://sketchers-server.onrender.com"
-    : "http://localhost:5000";
-console.log(isProd, "isProd");
-
-export const socket = io(isProd);
+    ? "https://sketchbook-server.onrender.com/"
+    : "http://localhost:5005";
+export const socket = io(URL);
