bc2dc16eb4e319de9afd53b9a59689afe4d948d1
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2024-02-24 16:36:14 +0530

    UPD: fixed bugs

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 const URL =
-  process.env.NODE_ENV === "production"
-    ? "https://sketchbook-server.onrender.com/"
-    : "http://localhost:5005";
+  process.env.NODE_ENV === 'production'
+    ? 'https://sketchbook-server.onrender.com/'
+    : 'http://localhost:5000';
+console.log(URL, process.env.NODE_ENV);
 export const socket = io(URL);
